982e07753276b53d07babd702b23fff0247e4fd9
Christoph Knoll <c.knoll@htblaleonding.onmicrosoft.com>
2020-05-28 15:38:11 +0200

    Add Asyncronous ReactiveX Ovservable/Observer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subscription, range, interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rx-demo';
+  ngOnInit(): void {
+    // Synchronous
+    // const observable$: Observable<number> = range(3, 5)
+    // const subscription: Subscription = observable$.subscribe(
+    //   (x) => console.log(observable$, 'Next: ' + x),
+    //   (err) => console.log('Error ' + err),
+    //   () => console.log('Completed')
+    // )
+
+    // Asynchronous
+    const observable$: Observable<number> = interval(1000)
+    const subscription: Subscription = observable$.subscribe(
+      (x) => console.log(observable$, 'Next: ' + x),
+      (err) => console.log('Error ' + err),
+      () => console.log('Completed')
+    )
+    
+  }
 }
