2ab5c3f5c785177d1245c4679222f1c0996321e4
Christoph Knoll <c.knoll@htblaleonding.onmicrosoft.com>
2020-05-28 15:49:20 +0200

    Add OnDestroy and unsubscribe from observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable, Subscription, range, interval } from 'rxjs';
 
 @Component({
@@ -6,8 +6,10 @@ import { Observable, Subscription, range, interval } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'rx-demo';
+  subscription: Subscription
+
   ngOnInit(): void {
     // Synchronous
     // const observable$: Observable<number> = range(3, 5)
@@ -19,11 +21,14 @@ export class AppComponent implements OnInit {
 
     // Asynchronous
     const observable$: Observable<number> = interval(1000)
-    const subscription: Subscription = observable$.subscribe(
+    this.subscription = observable$.subscribe(
       (x) => console.log(observable$, 'Next: ' + x),
       (err) => console.log('Error ' + err),
       () => console.log('Completed')
     )
-    
+
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
